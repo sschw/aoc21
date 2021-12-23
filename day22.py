bd7790795717ac36224f9f2039c24287eb25ce57
sschw <sylphitus@gmail.com>
2021-12-23 18:13:31 +0100

    Non working approach.

--- day22.py
+++ day22.py
@@ -20,64 +20,46 @@ for i, j in cube.items():
     sum += 1
 print(sum)
 
-def combine(cuboid1, cuboid2):
-  cuboids = [cuboid1] # keep first one
+def getOverlapCuboid(cuboid1, cuboid2):
   overlapXStart = max(cuboid1[1][0], cuboid2[1][0])
   overlapXEnd = min(cuboid1[1][1], cuboid2[1][1])
   overlapYStart = max(cuboid1[2][0], cuboid2[2][0])
   overlapYEnd = min(cuboid1[2][1], cuboid2[2][1])
   overlapZStart = max(cuboid1[3][0], cuboid2[3][0])
   overlapZEnd = min(cuboid1[3][1], cuboid2[3][1])
-  xPos = [cuboid2[1][0], overlapXStart, overlapXEnd, cuboid2[1][1]]
-  yPos = [cuboid2[2][0], overlapYStart, overlapYEnd, cuboid2[2][1]]
-  zPos = [cuboid2[3][0], overlapZStart, overlapZEnd, cuboid2[3][1]]
-  for x in range(0, len(xPos)-1):
-    for y in range(0, len(yPos)-1):
-      for z in range(0, len(zPos)-1):
-        if xPos == 1 and yPos == 1 and zPos == 1:
-          # overlapping rectangle, we already have this.
-          pass
-        elif xPos[x] >= xPos[x+1] or yPos[y] >= yPos[y+1] or zPos[z] >= zPos[z+1]:
-          # no overlapping.
-          pass
-        else:
-          cuboids.append((cuboid2[0], (xPos[x], xPos[x+1]), (yPos[x], yPos[x+1]), (zPos[x], zPos[x+1])))
-  return cuboids
+  return (None, (overlapXStart, overlapXEnd), (overlapYStart, overlapYEnd), (overlapZStart, overlapZEnd))
 
-def remove(cuboid1, cuboid2):
-  cuboids = []
+def getNrOfOverlaps(cuboid1, cuboid2):
   overlapXStart = max(cuboid1[1][0], cuboid2[1][0])
   overlapXEnd = min(cuboid1[1][1], cuboid2[1][1])
   overlapYStart = max(cuboid1[2][0], cuboid2[2][0])
   overlapYEnd = min(cuboid1[2][1], cuboid2[2][1])
   overlapZStart = max(cuboid1[3][0], cuboid2[3][0])
   overlapZEnd = min(cuboid1[3][1], cuboid2[3][1])
-  xPos = [cuboid1[1][0], overlapXStart, overlapXEnd, cuboid1[1][1]]
-  yPos = [cuboid1[2][0], overlapYStart, overlapYEnd, cuboid1[2][1]]
-  zPos = [cuboid1[3][0], overlapZStart, overlapZEnd, cuboid1[3][1]]
-  for x in range(0, len(xPos)-1):
-    for y in range(0, len(yPos)-1):
-      for z in range(0, len(zPos)-1):
-        if xPos == 1 and yPos == 1 and zPos == 1:
-          # overlapping rectangle, we don't want this one anymore.
-          pass
-        elif xPos[x] >= xPos[x+1] or yPos[y] >= yPos[y+1] or zPos[z] >= zPos[z+1]:
-          # no overlapping.
-          pass
-        else:
-          cuboids.append((cuboid2[0], (xPos[x], xPos[x+1]), (yPos[x], yPos[x+1]), (zPos[x], zPos[x+1])))
+  
+  overlapsX = overlapXEnd+1-overlapXStart
+  overlapsY = overlapYEnd+1-overlapYStart
+  overlapsZ = overlapZEnd+1-overlapZStart
+  if overlapsX <= 0 or overlapsY <= 0 or overlapsZ <= 0:
+    return 0
+  return overlapsX*overlapsY*overlapsZ
+  
 
 def sumCuboids(cuboids):
   sum = 0
-  for i in cuboids:
-    sum += (i[0][1] - i[0][0]) * (i[1][1] - i[1][0]) * (i[2][1] - i[2][0])
+  for i in range(0, len(cuboids)):
+    cub = cuboids[i]
+    if cub[0]:
+      sum += (cub[1][1]+1 - cub[1][0]) * (cub[2][1]+1 - cub[2][0]) * (cub[3][1]+1 - cub[3][0])
+    for j in range(0, i):
+      pcub = cuboids[j]
+      if pcub[0]:
+        sum -= getNrOfOverlaps(cub, pcub)
+      else:
+        sum += getNrOfOverlaps(cub, pcub)
   return sum
 
-cuboids = [input_per_line[0]]
-for c in input_per_line[1:]:
-  if c[0] == False:
-    newCuboids = []
-    for c2 in cuboids:
+print(sumCuboids(input_per_line))
 
 
 '''sumOn = 0
